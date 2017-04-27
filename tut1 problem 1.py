d2188cd433d842778db253d544631d3c9fbe4769
noziphoP <prudencekhabazela@gmail.com>
2017-04-27 18:24:14 +0200

    Add files via upload

---
+++ tut1 problem 1.py
@@ -0,0 +1,10 @@
+
+name= ('Nozipho P Mkhize')
+email= ('211533208@stu.ukzn.co.za')
+MyProject= ('Studying magnetic proparties of ferrit Neno-particles')
+toLearn= ('How to balance Chemical equations and to calculate surface area of nano-particles')
+print ('My name is ' + (name) )
+print ('My Email adress is ' + (email) )
+print ('My super awesome project is ' + (MyProject))
+print ('I would like to learn how to ' + (toLearn))
+
